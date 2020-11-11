ea10300a3f7b431370fc7db4b693af8d2c8aaee5
nrbhushan <rashtrabhushan2@gmail.com>
2020-11-11 12:01:37 +0530

    home component

---
+++ src/components/home/home.js
@@ -0,0 +1,17 @@
+export default  {
+    name: 'HomeComponent',
+    data() {
+        return {
+            msg: 'Hello Welcome to my app'
+        }
+    },
+    methods: {
+        getLength(str) {return str.length;}     
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+}
---
+++ src/components/home/home.vue
@@ -0,0 +1,7 @@
+<script src="./home.js"></script>
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <h3>{{getLength(msg)}}</h3>
+    </div>
+</template>
