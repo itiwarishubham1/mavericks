631d2c718d714fdb138df63a1b1a1ceddd339be2
nrbhushan <rashtrabhushan2@gmail.com>
2020-11-11 18:01:35 +0530

    slider

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,45 +3,27 @@ export default  {
     data() {
         return {
             msg: 'Hello Welcome to my app',
-            // progressBar: document.getElementById('progress-bar'),
+            currentDuration:0.1,
+            duration: 0,
+            steps:0.5,
         }
+
     },
     methods: {
-        progresClicked() {
-
-            // let player = this.$refs['video-element'];
-            let progressBar= this.$refs['progress-bar'];
-            // let fillbar = this.$refs['fill'];
-            // const handle = this.$refs['handle'];
-            progressBar.addEventListener('mousemove',this.seek);
-            // var percent = e.offsetX / progressBar.offsetWidth;
-            // player.currentTime = percent * player.duration;
-            // e.target.value = Math.floor(percent / 100);
-            // fillbar.style.width = percent + e.offsetX+'px';
-            // handle.style.left = percent + e.offsetX + 'px';
-        },
-        remove() {
-            let progressBar= this.$refs['progress-bar'];
-            progressBar.removeEventListener('mousemove');
-        },
-        seek(e) {
-            let player = this.$refs['video-element'];
-            let progressBar= this.$refs['progress-bar'];
-            let fillbar = this.$refs['fill'];
-            const handle = this.$refs['handle'];
-            // progressBar.addEventListener('mousemove',this.seek)
-            var percent = e.offsetX / progressBar.offsetWidth;
-            player.currentTime = percent * player.duration;
-            e.target.value = Math.floor(percent / 100);
-            fillbar.style.width = percent + e.offsetX+'px';
-            handle.style.left = percent + e.offsetX + 'px';
-        },
         getLength(str) {return str.length;}     
     },
     computed: {
         
     },
+    mounted() {
+        let player = this.$refs['video-element'];
+        this.duration = player.duration;
+    },
     watch: {
-        
+        currentDuration:function(val) {
+            let player = this.$refs['video-element'];
+            player.currentTime = val
+            this.currentDuration = player.currentTime;
+        }
     },
 }
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -3,14 +3,12 @@
 <template>
     <div>
         <div id="player">
-            <video ref="video-element"  width="500px" height="500px" >
+            <video ref="video-element"  width="500px" height="500px">
                 <source src="./video/test.mp4" type="video/mp4"/>
             </video>
             <div id="controls">
-                <div ref="progress-bar" class="progress-bar" @mousedown="progresClicked($event)" @mouseup="remove()">
-                    <div ref="fill" class="fill"></div>
-                    <div ref="handle" class="handle"></div>
-                    
+                <div>
+                    <input ref="slider" class="slider" type="range" min="0" :max="duration" :step="steps" v-model="currentDuration" />
                 </div>
             </div>
         </div>
