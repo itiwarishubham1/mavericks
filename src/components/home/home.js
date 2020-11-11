8cb573c9662c6fce9181901ca55a3bde0fd3e4e3
nrbhushan <rashtrabhushan2@gmail.com>
2020-11-11 16:24:32 +0530

    video component

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,16 +2,46 @@ export default  {
     name: 'HomeComponent',
     data() {
         return {
-            msg: 'Hello Welcome to my app'
+            msg: 'Hello Welcome to my app',
+            // progressBar: document.getElementById('progress-bar'),
         }
     },
     methods: {
+        progresClicked() {
+
+            // let player = this.$refs['video-element'];
+            let progressBar= this.$refs['progress-bar'];
+            // let fillbar = this.$refs['fill'];
+            // const handle = this.$refs['handle'];
+            progressBar.addEventListener('mousemove',this.seek);
+            // var percent = e.offsetX / progressBar.offsetWidth;
+            // player.currentTime = percent * player.duration;
+            // e.target.value = Math.floor(percent / 100);
+            // fillbar.style.width = percent + e.offsetX+'px';
+            // handle.style.left = percent + e.offsetX + 'px';
+        },
+        remove() {
+            let progressBar= this.$refs['progress-bar'];
+            progressBar.removeEventListener('mousemove');
+        },
+        seek(e) {
+            let player = this.$refs['video-element'];
+            let progressBar= this.$refs['progress-bar'];
+            let fillbar = this.$refs['fill'];
+            const handle = this.$refs['handle'];
+            // progressBar.addEventListener('mousemove',this.seek)
+            var percent = e.offsetX / progressBar.offsetWidth;
+            player.currentTime = percent * player.duration;
+            e.target.value = Math.floor(percent / 100);
+            fillbar.style.width = percent + e.offsetX+'px';
+            handle.style.left = percent + e.offsetX + 'px';
+        },
         getLength(str) {return str.length;}     
     },
     computed: {
-
+        
     },
     watch: {
-
+        
     },
 }
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,7 +1,18 @@
 <script src="./home.js"></script>
+<style scoped src="./home.css"></style>
 <template>
     <div>
-        <h2>{{msg}}</h2>
-        <h3>{{getLength(msg)}}</h3>
+        <div id="player">
+            <video ref="video-element"  width="500px" height="500px" >
+                <source src="./video/test.mp4" type="video/mp4"/>
+            </video>
+            <div id="controls">
+                <div ref="progress-bar" class="progress-bar" @mousedown="progresClicked($event)" @mouseup="remove()">
+                    <div ref="fill" class="fill"></div>
+                    <div ref="handle" class="handle"></div>
+                    
+                </div>
+            </div>
+        </div>
     </div>
 </template>
