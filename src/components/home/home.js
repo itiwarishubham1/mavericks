ace54eb604eff4c1d4ef60c6b57b925b442f176f
nrbhushan <rashtrabhushan2@gmail.com>
2020-11-11 18:44:05 +0530

    css fix

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,7 +4,7 @@ export default  {
         return {
             msg: 'Hello Welcome to my app',
             currentDuration:0.1,
-            duration: 0,
+            duration: 100,
             steps:0.5,
         }
 
@@ -15,15 +15,14 @@ export default  {
     computed: {
         
     },
-    mounted() {
-        let player = this.$refs['video-element'];
-        this.duration = player.duration;
-    },
     watch: {
         currentDuration:function(val) {
             let player = this.$refs['video-element'];
+            let slider = this.$refs['slider'];
+            this.duration = player.duration;
             player.currentTime = val
             this.currentDuration = player.currentTime;
+            slider.style.background = 'linear-gradient(to right, #82CFD0 0px, #82CFD0 ' + val*(400/this.duration) + 'px, black ' + val*(400/this.duration) + 'px, black 400px)'
         }
     },
 }
