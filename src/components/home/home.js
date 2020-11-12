c4b6918b02b75594f77d9610babf603481a05288
Shubhanshu Saxena <Shubhanshu.Saxena@openlane.com>
2020-11-12 17:11:12 +0530

    Magnifier.

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,3 +1,5 @@
+const Magnifier = require("./Magnifier");
+
 export default  {
     name: 'HomeComponent',
     data() {
@@ -11,10 +13,83 @@ export default  {
 
     },
     methods: {
+        magnify(imgID, zoom) {
+            var img, glass, w, h, bw;
+            img = document.getElementById(imgID);
+            /*create magnifier glass:*/
+            glass = document.createElement("DIV");
+            glass.setAttribute("class", "img-magnifier-glass");
+            /*insert magnifier glass:*/
+            img.parentElement.insertBefore(glass, img);
+            /*set background properties for the magnifier glass:*/
+            glass.style.backgroundImage = "url('" + img.src + "')";
+            glass.style.backgroundRepeat = "no-repeat";
+            glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+            bw = 3;
+            w = glass.offsetWidth / 2;
+            h = glass.offsetHeight / 2;
+            console.log('width'+w);
+            /*execute a function when someone moves the magnifier glass over the image:*/
+            glass.addEventListener("mousemove", moveMagnifier);
+            img.addEventListener("mousemove", moveMagnifier);
+            /*and also for touch screens:*/
+            glass.addEventListener("touchmove", moveMagnifier);
+            img.addEventListener("touchmove", moveMagnifier);
+            function moveMagnifier(e) {
+              var pos, x, y;
+              /*prevent any other actions that may occur when moving over the image*/
+              e.preventDefault();
+              /*get the cursor's x and y positions:*/
+              pos = getCursorPos(e);
+              x = pos.x;
+              y = pos.y;
+              console.log('x:'+x);
+              console.log('y:'+y);
+              /*prevent the magnifier glass from being positioned outside the image:*/
+              if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+              if (x < w / zoom) {x = w / zoom;}
+              if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+              if (y < h / zoom) {y = h / zoom;}
+              /*set the position of the magnifier glass:*/
+              glass.style.left = (x - w) + "px";
+              glass.style.top = (y - h) + "px";
+              /*display what the magnifier glass "sees":*/
+              glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+            }
+            function getCursorPos(e) {
+              var a, x = 0, y = 0;
+              e = e || window.event;
+              /*get the x and y positions of the image:*/
+              a = img.getBoundingClientRect();
+              /*calculate the cursor's x and y coordinates, relative to the image:*/
+              x = e.pageX - a.left;
+              y = e.pageY - a.top;
+              /*consider any page scrolling:*/
+              x = x - window.pageXOffset;
+              y = y - window.pageYOffset;
+              return {x : x, y : y};
+            }
+        },
         togglePlay() {
             let player = this.$refs['video-element'];
             if (this.isPlaying) {
                 player.pause();
+                var canvas = document.createElement("canvas");
+                var video = document.getElementById("videoPlayer");
+                if (video.paused) {
+                    var ctx = canvas.getContext('2d');
+                    ctx.scale(1,1);
+                    ctx.drawImage(video, 0, 0, 500, 500);
+                }
+                var img = document.getElementById("thumb");
+                img.src = canvas.toDataURL(); 
+                //this.magnify("myimage", 2);
+                //let m = Magnifier();
+                Magnifier.attach({
+                    thumb: '#thumb',
+                    large: 'http://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Starry_Night_Over_the_Rhone.jpg/400px-Starry_Night_Over_the_Rhone.jpg',
+                    largeWrapper: 'preview'
+                });
             } else {
                 player.play();
             }
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,16 +1,39 @@
-<script src="./home.js"></script>
 <style scoped src="./home.css"></style>
+<style src="./magnifier.css"></style>
+<script src="./home.js"></script>
+
 <template>
-    <div>
-        <div id="player">
-            <video ref="video-element"  width="500px" height="500px" @click="togglePlay($event)" @timeupdate="updateDuration($event)">
-                <source src="./video/test.mp4" type="video/mp4"/>
-            </video>
-            <div id="controls">
-                <div>
-                    <input ref="slider" class="slider" type="range" min="0" :max="duration" :step="steps" v-model="currentDuration" />
-                </div>
-            </div>
+  <div>
+    <div id="player">
+      <video
+        id="videoPlayer"
+        ref="video-element"
+        width="500px"
+        height="500px"
+        @click="togglePlay($event)"
+        @timeupdate="updateDuration($event)"
+      >
+        <source src="./video/test.mp4" type="video/mp4" />
+      </video>
+      <div id="controls">
+        <div>
+          <input
+            ref="slider"
+            class="slider"
+            type="range"
+            min="0"
+            :max="duration"
+            :step="steps"
+            v-model="currentDuration"
+          />
         </div>
+      </div>
     </div>
+    <div>
+    <div class="magnifier-thumb-wrapper" href="http://en.wikipedia.org/wiki/File:Starry_Night_Over_the_Rhone.jpg">
+        <img id="thumb" src="http://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Starry_Night_Over_the_Rhone.jpg/200px-Starry_Night_Over_the_Rhone.jpg">
+    </div>
+    <div class="magnifier-preview" id="preview" style="width: 200px; height: 133px">Starry Night Over The Rhone<br>by Vincent van Gogh</div>
+</div>
+  </div>
 </template>
