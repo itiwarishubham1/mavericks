2f3a2ef37aab72b03c48557ce8b3d6a6c0ec9755
Shubhanshu Saxena <Shubhanshu.Saxena@openlane.com>
2020-11-12 17:31:46 +0530

    Magnifier base.

--- src/components/home/Magnifier.js
+++ src/components/home/Magnifier.js
@@ -1,62 +1,4 @@
-var evt = function () {
-    "use strict";
-    this.attach = function (evtName, element, listener, capture) {
-        var evt         = '',
-            useCapture  = (capture === undefined) ? true : capture,
-            handler     = null;
-
-        if (window.addEventListener === undefined) {
-            evt = 'on' + evtName;
-            handler = function (evt, listener) {
-                element.attachEvent(evt, listener);
-                return listener;
-            };
-        } else {
-            evt = evtName;
-            handler = function (evt, listener, useCapture) {
-                element.addEventListener(evt, listener, useCapture);
-                return listener;
-            };
-        }
-
-        return handler.apply(element, [evt, function (ev) {
-            var e   = ev || event,
-                src = e.srcElement || e.target;
-
-            listener(e, src);
-        }, useCapture]);
-    };
-
-    this.detach = function (evtName, element, listener, capture) {
-        var evt         = '',
-            useCapture  = (capture === undefined) ? true : capture;
-
-        if (window.removeEventListener === undefined) {
-            evt = 'on' + evtName;
-            element.detachEvent(evt, listener);
-        } else {
-            evt = evtName;
-            element.removeEventListener(evt, listener, useCapture);
-        }
-    };
-
-    this.stop = function (evt) {
-        evt.cancelBubble = true;
-
-        if (evt.stopPropagation) {
-            evt.stopPropagation();
-        }
-    };
-
-    this.prevent = function (evt) {
-        if (evt.preventDefault) {
-            evt.preventDefault();
-        } else {
-            evt.returnValue = false;
-        }
-    };
-}; 
-var Magnifier = function (options) {
+var Magnifier = function (evt, options) {
     "use strict";
 
     var gOptions = options || {},
@@ -624,4 +566,4 @@ var Magnifier = function (options) {
         }
     });
 };
-export default new Magnifier();
+export default Magnifier;
